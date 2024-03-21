dd82c48f9e5a5cd41345e178ccab06c40ce04c83
AzzamSyakir <azzamsykir@gmail.com>
2024-03-22 05:21:54 +0700

    feat: add repository layer

---
+++ internal/repository/product_repository.go
@@ -0,0 +1,57 @@
+package repository
+
+import (
+	"database/sql"
+	"go-micro-services/internal/entity"
+)
+
+type ProductRepository struct{}
+
+func NewProductRepository() *ProductRepository {
+	productRepository := &ProductRepository{}
+	return productRepository
+}
+func (productRepository ProductRepository) GetOneById(tx *sql.Tx, id string) (result *entity.Product, err error) {
+	var rows *sql.Rows
+	var queryErr error
+	rows, queryErr = tx.Query(
+		`SELECT id, name, price, stock created_at, updated_at, deleted_at FROM "products" WHERE id=$1 LIMIT 1;`,
+		id,
+	)
+	if queryErr != nil {
+		result = nil
+		err = queryErr
+		return result, err
+	}
+
+	foundProducts := DeserializeProductRows(rows)
+	if len(foundProducts) == 0 {
+		result = nil
+		err = nil
+		return result, err
+	}
+
+	result = foundProducts[0]
+	err = nil
+	return result, err
+}
+func DeserializeProductRows(rows *sql.Rows) []*entity.Product {
+	var foundProducts []*entity.Product
+	for rows.Next() {
+		foundProduct := &entity.Product{}
+		scanErr := rows.Scan(
+			&foundProduct.Id,
+			&foundProduct.Name,
+			&foundProduct.Price,
+			&foundProduct.Stock,
+			&foundProduct.CreatedAt,
+			&foundProduct.UpdatedAt,
+			&foundProduct.DeletedAt,
+		)
+		if scanErr != nil {
+			panic(scanErr)
+		}
+		foundProducts = append(foundProducts, foundProduct)
+	}
+	return foundProducts
+}
---
+++ internal/repository/user_repository.go
@@ -0,0 +1,58 @@
+package repository
+
+import (
+	"database/sql"
+	"go-micro-services/internal/entity"
+)
+
+type UserRepository struct {
+}
+
+func NewUserRepository() *UserRepository {
+	userRepository := &UserRepository{}
+	return userRepository
+}
+func (userRepository *UserRepository) GetOneById(tx *sql.Tx, id string) (result *entity.User, err error) {
+	var rows *sql.Rows
+	var queryErr error
+	rows, queryErr = tx.Query(
+		`SELECT id, name, saldo created_at, updated_at, deleted_at FROM "user" WHERE id=$1 LIMIT 1;`,
+		id,
+	)
+	if queryErr != nil {
+		result = nil
+		err = queryErr
+		return result, err
+	}
+
+	foundUsers := DeserializeUserRows(rows)
+	if len(foundUsers) == 0 {
+		result = nil
+		err = nil
+		return result, err
+	}
+
+	result = foundUsers[0]
+	err = nil
+	return result, err
+}
+
+func DeserializeUserRows(rows *sql.Rows) []*entity.User {
+	var foundUsers []*entity.User
+	for rows.Next() {
+		foundUser := &entity.User{}
+		scanErr := rows.Scan(
+			&foundUser.Id,
+			&foundUser.Name,
+			&foundUser.Saldo,
+			&foundUser.CreatedAt,
+			&foundUser.UpdatedAt,
+			&foundUser.DeletedAt,
+		)
+		if scanErr != nil {
+			panic(scanErr)
+		}
+		foundUsers = append(foundUsers, foundUser)
+	}
+	return foundUsers
+}
