0251445efa06b9554e6b60806f923843b0c5cf28
AzzamSyakir <azzamsykir@gmail.com>
2024-03-26 16:24:13 +0700

    fix: fixed user route

--- internal/delivery/http/route/user_route.go
+++ internal/delivery/http/route/user_route.go
@@ -20,5 +20,5 @@ func NewUserRoute(router *mux.Router, userController *http.UserController) *User
 
 func (userRoute *UserRoute) Register() {
 	userRoute.Router.HandleFunc("/{id}", userRoute.UserController.GetOneById).Methods("GET")
-	userRoute.Router.HandleFunc("/{id}", userRoute.UserController.PatchOneById).Methods("PATCH")
+	userRoute.Router.HandleFunc("/update-balance/{id}", userRoute.UserController.PatchOneById).Methods("PATCH")
 }
