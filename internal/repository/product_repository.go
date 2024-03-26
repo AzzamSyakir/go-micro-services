d1ea8d3d058a69087352bc3be410e0b73396d953
AzzamSyakir <azzamsykir@gmail.com>
2024-03-26 16:23:44 +0700

    feat: add product PatchOneById function repository

--- internal/repository/product_repository.go
+++ internal/repository/product_repository.go
@@ -11,6 +11,26 @@ func NewProductRepository() *ProductRepository {
 	productRepository := &ProductRepository{}
 	return productRepository
 }
+func DeserializeProductRows(rows *sql.Rows) []*entity.Product {
+	var foundProducts []*entity.Product
+	for rows.Next() {
+		foundProduct := &entity.Product{}
+		scanErr := rows.Scan(
+			&foundProduct.Id,
+			&foundProduct.Name,
+			&foundProduct.Price,
+			&foundProduct.Stock,
+			&foundProduct.CreatedAt,
+			&foundProduct.UpdatedAt,
+			&foundProduct.DeletedAt,
+		)
+		if scanErr != nil {
+			panic(scanErr)
+		}
+		foundProducts = append(foundProducts, foundProduct)
+	}
+	return foundProducts
+}
 func (productRepository ProductRepository) GetOneById(tx *sql.Tx, id string) (result *entity.Product, err error) {
 	var rows *sql.Rows
 	var queryErr error
@@ -35,23 +55,23 @@ func (productRepository ProductRepository) GetOneById(tx *sql.Tx, id string) (re
 	err = nil
 	return result, err
 }
-func DeserializeProductRows(rows *sql.Rows) []*entity.Product {
-	var foundProducts []*entity.Product
-	for rows.Next() {
-		foundProduct := &entity.Product{}
-		scanErr := rows.Scan(
-			&foundProduct.Id,
-			&foundProduct.Name,
-			&foundProduct.Price,
-			&foundProduct.Stock,
-			&foundProduct.CreatedAt,
-			&foundProduct.UpdatedAt,
-			&foundProduct.DeletedAt,
-		)
-		if scanErr != nil {
-			panic(scanErr)
-		}
-		foundProducts = append(foundProducts, foundProduct)
+func (productRepository *ProductRepository) PatchOneById(begin *sql.Tx, id string, toPatchProduct *entity.Product) (result *entity.Product, err error) {
+	_, queryErr := begin.Query(
+		`UPDATE "products" SET name=$1,  stock=$2, price=$3, updated_at=$4 WHERE id = $5 ;`,
+		toPatchProduct.Name,
+		toPatchProduct.Stock,
+		toPatchProduct.Price,
+		toPatchProduct.UpdatedAt,
+		id,
+	)
+
+	if queryErr != nil {
+		result = nil
+		err = queryErr
+		return
 	}
-	return foundProducts
+
+	result = toPatchProduct
+	err = nil
+	return result, err
 }
