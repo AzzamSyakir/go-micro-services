12a35a858917accb179a2baa6a98c3541bb0f9b1
Azzamsyakir <azzamsykir@gmail.com>
2024-04-02 13:55:48 +0700

    fix: fixed entity/product

--- internal/delivery/http/order_controller.go
+++ internal/delivery/http/order_controller.go
@@ -1,6 +1,13 @@
 package http
 
-import "go-micro-services/internal/use_case"
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	model_request "go-micro-services/internal/model/request/controller"
+	"go-micro-services/internal/model/response"
+	"go-micro-services/internal/use_case"
+	"net/http"
+)
 
 type OrderController struct {
 	OrderUseCase *use_case.OrderUseCase
@@ -11,4 +18,20 @@ func NewOrderController(orderUseCase *use_case.OrderUseCase) *OrderController {
 		OrderUseCase: orderUseCase,
 	}
 	return orderControler
+}
+
+func (orderController *OrderController) Orders(writer http.ResponseWriter, reader *http.Request) {
+	vars := mux.Vars(reader)
+	userId := vars["id"]
+	request := model_request.OrderRequest{}
+
+	decodeErr := json.NewDecoder(reader.Body).Decode(request)
+
+	if decodeErr != nil {
+		panic(decodeErr)
+	}
+
+	result := orderController.OrderUseCase.Order(userId, request)
+	response.NewResponse(writer, result)
+
 }
--- internal/entity/product.go
+++ internal/entity/product.go
@@ -1,15 +1,15 @@
 package entity
 
-import (
-	"github.com/guregu/null"
-)
+import "github.com/guregu/null"
 
 type Product struct {
-	Id        null.String `json:"id" bson:"id"`
-	Name      null.String `json:"name" bson:"name"`
-	Price     null.String `json:"Price" bson:"price"`
-	Stock     null.Int    `json:"stock" bson:"stock"`
-	CreatedAt null.Time   `json:"created_at" bson:"created_at"`
-	UpdatedAt null.Time   `json:"updated_at" bson:"updated_at"`
-	DeletedAt null.Time   `json:"deleted_at" bson:"deleted_at"`
+	Id         null.String `json:"id"`
+	Sku        null.String `json:"sku"`
+	Name       null.String `json:"name"`
+	Stock      null.Int    `json:"stock"`
+	Price      null.String `json:"price"`
+	CategoryId null.String `json:"category_id"`
+	CreatedAt  null.Time   `json:"created_at"`
+	UpdatedAt  null.Time   `json:"updated_at"`
+	DeletedAt  null.Time   `json:"deleted_at"`
 }
