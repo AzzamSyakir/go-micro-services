24bf7d42802e4a00f9ce98bccc2c3d7bbca2c560
Azzamsyakir <azzamsykir@gmail.com>
2024-04-02 22:46:38 +0700

    fixed product entity

--- internal/entity/product.go
+++ internal/entity/product.go
@@ -7,7 +7,7 @@ type Product struct {
 	Sku        null.String `json:"sku"`
 	Name       null.String `json:"name"`
 	Stock      null.Int    `json:"stock"`
-	Price      null.String `json:"price"`
+	Price      null.Int    `json:"price"`
 	CategoryId null.String `json:"category_id"`
 	CreatedAt  null.Time   `json:"created_at"`
 	UpdatedAt  null.Time   `json:"updated_at"`
