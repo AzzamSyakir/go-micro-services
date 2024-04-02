c760fd3e2896e323ced776be2ebf91faabcc1e38
Azzamsyakir <azzamsykir@gmail.com>
2024-04-02 13:56:21 +0700

    fix: fixed request orderRequest

--- internal/model/request/controller/order_request.go
+++ internal/model/request/controller/order_request.go
@@ -2,11 +2,14 @@ package request
 
 import "github.com/guregu/null"
 
-type Products struct {
-	ProductId null.String `json:"product_id"`
-	Qty       null.Int    `json:"qty"`
+type OrderProducts struct {
+	Id         null.String `json:"id"`
+	OrderId    null.String `json:"order_id"`
+	ProductId  null.String `json:"product_id"`
+	Qty        null.Int    `json:"qty"`
+	TotalPrice null.Int    `json:"total_price"`
 }
 type OrderRequest struct {
-	TotalPaid null.String `json:"name"`
-	Products  Products    `json:"products"`
+	TotalPaid null.Int        `json:"total_price"`
+	Products  []OrderProducts `json:"products"`
 }
