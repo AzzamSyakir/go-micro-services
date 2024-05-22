2980ca219147de63e3a9460194373c9234a9b38c
Azzamsyakir <azzamsykir@gmail.com>
2024-05-22 18:45:59 +0700

    feat: intialize client to communicate to other service with grpc

---
+++ src/order-service/client/user_client.go
@@ -0,0 +1,27 @@
+package client
+
+import (
+	"fmt"
+
+	"go-micro-services/src/order-service/delivery/grpc/pb"
+
+	"google.golang.org/grpc"
+)
+
+type ProductServiceClient struct {
+	Client pb.ProductServiceClient
+}
+
+func InitProductServiceClient(url string) ProductServiceClient {
+	cc, err := grpc.Dial(url, grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Println("Could not connect:", err)
+	}
+
+	c := ProductServiceClient{
+		Client: pb.NewProductServiceClient(cc),
+	}
+
+	return c
+}
